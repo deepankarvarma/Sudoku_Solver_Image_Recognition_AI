cd84ae3be2d98a3d7225b958ff05b25f6392d5be
deepankarvarma <satwikdpshrit@gmail.com>
2022-07-15 01:08:30 +0530

    addedlogo to tkinter window

--- sudokusolver.py
+++ sudokusolver.py
@@ -1,3 +1,6 @@
+from tkinter import Tk
+
+
 rows, cols=9,9
 def find(lst,searchEle):
     (i,j) = (-1, -1)
@@ -32,9 +35,14 @@ def sudokuSolver(lst):
             lst[i][j]=0
 
 def main():
+    root=Tk()
+    root.title('Sudoku Solver using Image')
+    root.iconbitmap('C:\\Users\\acer\\Desktop\\Thapar\\Subjects\\4th Semester\\Artificial Intelligence UCS411\\Project\\AI Project Deepankar Varma ,Prateek Sharawat\\logo.ico')
+    root.geometry("400x400")
     # lst=eval(input('Enter the list (0 for missing values): '))
     lst=[[5,3,0,0,7,0,0,0,0],[6,0,0,1,9,5,0,0,0],[0,9,8,0,0,0,0,6,0],[8,0,0,0,6,0,0,0,3],[4,0,0,8,0,3,0,0,1],[7,0,0,0,2,0,0,0,6],[0,6,0,0,0,0,2,8,0],[0,0,0,4,1,9,0,0,5],[0,0,0,0,8,0,0,7,9]]
     sudokuSolver(lst)
+    root.mainloop()
 
 if __name__=='__main__':
     main()
